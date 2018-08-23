ebb9434478fef71a1de19a8da7998979cdbdc117
Terry Teng <terry.teng@oocl.com>
2018-08-23 14:21:47 +0800

    [terry louis] refactor

--- src/main/java/com/demo/tdd/Permutation.java
+++ src/main/java/com/demo/tdd/Permutation.java
@@ -8,32 +8,20 @@ import java.util.List;
 public class Permutation {
     public List<String> cal(String input) {
         List<String> list = new ArrayList<>();
-        if (input.length() == 2) {
-            list.add(input.substring(0, 1) + input.substring(1, 2));
-            list.add(input.substring(1, 2) + input.substring(0, 1));
-            return list;
-        }
-        if(input.length() == 3) {
-            list.add(input);
-            list.add(input.substring(0,1) + input.substring(2,3) + input.substring(1,2));
-            list.add(input.substring(1,2) + input.substring(0,1) + input.substring(2,3));
-            list.add(input.substring(1,2) + input.substring(2,3) + input.substring(0,1));
-            list.add(input.substring(2,3) + input.substring(0,1) + input.substring(1,2));
-            list.add(input.substring(2,3) + input.substring(1,2) + input.substring(0,1));
-            return list;
-        }
-        if(input.length()>3){
+        if(input.length()>1){
             for(int i = 0; i < input.length(); i++){
-                String sub = input.substring(0, i) + input.substring(i+1);
+                String sub = get(input, i);
                 List<String> res = cal(sub);
                 for(String str : res){
                     list.add(input.charAt(i)+str);
                 }
-
             }
             return list;
         }
         return Arrays.asList(input);
     }
 
+    private String get(String input, int index){
+        return input.substring(0, index) + input.substring(index+1);
+    }
 }
