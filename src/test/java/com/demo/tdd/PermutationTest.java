c784138b32126d0cd72f638df1532d50d048c636
Terry Teng <terry.teng@oocl.com>
2018-08-23 13:48:21 +0800

    [terry louis] should return ab ba when input ab fail

--- src/test/java/com/demo/tdd/PermutationTest.java
+++ src/test/java/com/demo/tdd/PermutationTest.java
@@ -2,6 +2,7 @@ package com.demo.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,5 +19,16 @@ public class PermutationTest {
         List<String> result = permutation.cal(input);
     // then
         assertThat(Arrays.asList(input), is(result));
+    }
+
+    @Test
+    public void should_return_ab_ba_when_input_ab(){
+    // given
+        String input = "ab";
+        Permutation permutation = new Permutation();
+    // when
+        List<String> result = permutation.cal(input);
+    // then
+        assertThat(Arrays.asList("ab","ba"), is(result));
     }
 }
---
+++ task.md
@@ -0,0 +1,7 @@
+1. given: a String "a"  
+   when : permutation.cal("a")  
+   then : return ["a"]
+
+2. given: a String "ab"  
+   when : permutation.cal("ab")  
+   then : return ["ab","ba"]
