424ad755d458ff5a47f682025aebf2537c00ab11
Terry Teng <terry.teng@oocl.com>
2018-08-23 13:53:40 +0800

    [terry louis] should_return_ab_ba_when_input_ab success

--- src/main/java/com/demo/tdd/Permutation.java
+++ src/main/java/com/demo/tdd/Permutation.java
@@ -1,11 +1,19 @@
 package com.demo.tdd;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Permutation {
     public List<String> cal(String input) {
-    return Arrays.asList(input);
+        List<String> list = new ArrayList<>();
+        if (input.length() == 2) {
+            list.add(input.substring(0, 1) + input.substring(1, 2));
+            list.add(input.substring(1, 2) + input.substring(0, 1));
+            return list;
+        }
+        return Arrays.asList(input);
     }
+
 }
